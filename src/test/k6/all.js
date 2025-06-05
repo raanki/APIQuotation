8f40058b0cc28e55257f48fbef26a429bef3cf45
ranki <ranki@efrei.net>
2025-06-05 21:39:29 +0200

    actions (github) : add load k6 test

---
+++ src/test/k6/all.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, group } from 'k6';
+
+export let options = {
+    vus: 50,
+    duration: '10s'
+};
+
+export default function () {
+    group('Test /quotations', function () {
+        const res = http.get('http://host.docker.internal:8080/quotations');
+        check(res, { 'status is 200': (r) => r.status === 200 });
+    });
+
+    group('Test /categories', function () {
+        const res = http.get('http://host.docker.internal:8080/categories');
+        check(res, { 'status is 200': (r) => r.status === 200 });
+    });
+
+    // group('Test /categories', function () {
+    //     const res = http.get('http://host.docker.internal:8080');
+    //     check(res, { 'status is 200': (r) => r.status === 200 });
+    // });
+}
