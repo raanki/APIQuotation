c002fe0fe492a3de275918b6628017b6de1aab0b
ranki <ranki@efrei.net>
2025-06-05 21:57:24 +0200

    fix (ci-cd) : load-test-k6

--- src/test/k6/all.js
+++ src/test/k6/all.js
@@ -8,12 +8,12 @@ export let options = {
 
 export default function () {
     group('Test /quotations', function () {
-        const res = http.get('http://host.docker.internal:8080/quotations');
+        const res = http.get('http://app:8080/quotations');
         check(res, { 'status is 200': (r) => r.status === 200 });
     });
 
     group('Test /categories', function () {
-        const res = http.get('http://host.docker.internal:8080/categories');
+        const res = http.get('http://app:8080/categories');
         check(res, { 'status is 200': (r) => r.status === 200 });
     });
 
